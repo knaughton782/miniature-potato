8f54f2eb255aeb25be229db6e70db3dc24326694
knaughton782 <kirsten.naughton@gmail.com>
2021-04-26 21:08:06 -0700

    working state, before breaking out into another component

--- udemy-react/src/components/ExpenseItem.js
+++ udemy-react/src/components/ExpenseItem.js
@@ -1,19 +1,26 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
-function ExpenseItem() {
-    const expenseDate = new Date(2021, 2, 28);
-    const expenseTitle = 'Car Insurance';
-    const expenseAmount = 294.67;
-
+function ExpenseItem(props) {
+    const month = props.date.toLocaleString('en-US', { month: 'long' });
+    const day = props.date.toLocaleString('en-US', { day: '2-digit' });
+    const year = props.date.getFullYear();
     return (
         <div className='expense-item'>
-            <div>{expenseDate.toISOString()}</div>
+
+            <div>
+                <div>{month}</div>
+                <div>{day}</div>
+                <div>{year}</div>
+            </div> 
+
+    {/* <ExpenseDate /> */}
+
             <div className='expense-item__description'>
-                <h2>{expenseTitle}</h2>
-                <div className='expense-item__price'>${expenseAmount}</div>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>${props.amount}</div>
             </div>
         </div>
-
 
     );
 }
