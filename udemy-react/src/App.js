d881d74946d9838961f52c165c8a05479d5d6f78
knaughton782 <kirsten.naughton@gmail.com>
2021-04-26 19:52:34 -0700

    progressing through section 2, variables in jsx

--- udemy-react/src/App.js
+++ udemy-react/src/App.js
@@ -1,26 +1,12 @@
-import React, { Component } from 'react';
-import './App.css';
-import Person from './Person/Person';
+import ExpenseItem from './components/ExpenseItem';
 
-
-class App extends Component {
-	state = {
-		persons: [ { name: 'Max', age: 28 }, { name: 'Manu', age: 26 }, { name: 'Susan', age: 22 } ]
-	};
-
-	render() {
-		return (
-			<div className="App">
-				<h1>I'm a custom React App</h1>
-				<p>This is JSX</p>
-				<Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-				<Person name={this.state.persons[1].name} age={this.state.persons[0].age} />
-				<Person name={this.state.persons[2].name} age={this.state.persons[0].age} />
-
-				<button>Switch Name</button>
-			</div>
-		);
-	}
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <ExpenseItem />
+    </div>
+  );
 }
 
 export default App;
--- udemy-react/src/index.js
+++ udemy-react/src/index.js
@@ -1,20 +1,6 @@
-import React from 'react'; // react library
-import ReactDOM from 'react-dom'; // glue between react elements and the dom
-import './index.css'; 
-import App from './App'; // app component
-import registerServiceWorker from './registerServiceWorker'; // faster loading in production
+import ReactDOM from 'react-dom';
 
+import './index.css';
+import App from './App';
 
-
-
-
-// const getCurrentDate = () => {
-//     const date = new Date();
-//     return date.toDateString();
-// }
-// const greeting = <h1>Current date: {getCurrentDate()}</h1>
-
-
-ReactDOM.render(<App />, document.getElementById('root')); //two arguments: app element and root id in html file
-// ReactDOM.render(greeting, document.getElementById('root')); //two arguments: app element and root id in html file
-registerServiceWorker();
+ReactDOM.render(<App />, document.getElementById('root'));
