b6bd17fd3152bc3e35337020acd5d1653025df88
knaughton782 <kirsten.naughton@gmail.com>
2021-04-27 15:32:10 -0700

    added ability to type in form and clear form on submit

--- udemy-react/src/components/NewExpense/ExpenseForm.js
+++ udemy-react/src/components/NewExpense/ExpenseForm.js
@@ -30,21 +30,33 @@ const ExpenseForm = () => {
         //     return { ...prevState, enteredDate: event.target.value};
         // });
     };
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        };
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
+    };
 
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title: </label>
-                    <input type="text" onChange={titleChangeHandler} />
+                    <input type="text" value={enteredTitle} onChange={titleChangeHandler} />
                 </div>
                 <div className="new-expense__control">
                     <label>Amount: </label>
-                    <input type="number" min='0.01' step='0.01' onChange={amountChangeHandler} />
+                    <input type="number" value={enteredAmount} min='0.01' step='0.01' onChange={amountChangeHandler} />
                 </div>
                 <div className="new-expense__control">
                     <label>Date: </label>
-                    <input type="date" min='2019-01-01' max='2022/12/31' onChange={dateChangeHandler} />
+                    <input type="date" value={enteredDate} min='2019-01-01' max='2022/12/31' onChange={dateChangeHandler} />
                 </div>
             </div>
             <div className='new-expense__actions'>
